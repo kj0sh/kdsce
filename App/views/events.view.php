d77b8119786b968462efc0644cbabeeaecd7dd97
KingJoshua <kingjayeriughe@gmail.com>
2026-02-01 12:48:39 +0100

    feat: add views for various pages

---
+++ App/views/events.view.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Our Events | Kaduna Student Career Expo - KDSCE</title>
+    <link rel="icon" type="image/x-icon" href="img/kdsce-icon.png">
+    <link rel="stylesheet" href="css/plugins.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+</head>
+
+<body>
+    <header class="position-absolute w-100 nav_header">
+        <nav class="rdn_nav bg-white shadow-sm">
+            <div class="container p-3">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-6"><a href="index.html">
+                            <img src="img/kdsce-logo-cropped-no-bg.png" class="img-fluid logo" alt=""></a></div>
+                    <div class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center gap-1">
+                        <ul class="list-unstyled fw-medium d-flex align-items-center justify-content-end m-0 gap-0">
+                            <li><a class="p-2" href="index.html">Home</a></li>
+                            <li><a class="p-2" href="about.html">About</a></li>
+                            <li><a class="p-2 tc_primary" href="events.html">Events</a></li>
+                            <li><a class="p-2" href="contact.html">Contact</a></li>
+                            <li><a class="p-2" href="">Blog</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 d-none d-lg-flex justify-content-end align-items-center">
+                        <ul class="list-unstyled head_white_social d-flex m-0 gap-2">
+                            <li><a href="https://www.linkedin.com/company/kaduna-student-career-expo"><i class="fa-brands fa-linkedin"></i></a></li>
+                            <li><a href="https://www.instagram.com/kaduna_student_career_expo"><i class="fa-brands fa-instagram"></i></a></li>
+                            <li><a href="https://twitter.com/kdsc_expo"><i class="fa-brands fa-x-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 justify-content-end d-flex d-lg-none"><a class="mobile_menu_btn flex" href="#!"><i
+                                class="fa fa-bars"></i></a>
+                        <div class="dark_mobile_menu mobile_menu_container offcanvas offcanvas-start" tabindex="-1"
+                            data-bs-scroll="true" id="mobilemenu">
+                            <div class="offcanvas-header"><button class="close_mb" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"><i class="ph-x"></i></button></div>
+                            <div class="offcanvas-body d-flex flex-column"><a href="index.html">
+                                    <img src="img/kdsce-logo-cropped-no-bg.png" alt="kdsce_logo" class="logo img-fluid"></a>
+                                <ul class="list-unstyled mobile_menu smoothscroll mb-auto">
+                                    <li><a class="p-2" href="index.html">Home</a></li>
+                                    <li><a class="p-2" href="about.html">About</a></li>
+                                    <li><a class="p-2 tc_primary" href="events.html">Events</a></li>
+                                    <li><a class="p-2" href="contact.html">Contact</a></li>
+                                    <li><a class="p-2" href="">Blog</a></li>
+                                </ul>
+                                </ul>
+                                <div class="d-flex flex-column">
+                                    <ul class="list-unstyled d-flex mb-0 gap-2">
+                                        <li><a href="https://www.linkedin.com/company/kaduna-student-career-expo"><i class="fa-brands fa-linkedin"></i></a></li>
+                                        <li><a href="https://www.instagram.com/kaduna_student_career_expo"><i class="fa-brands fa-instagram"></i></a></li>
+                                        <li><a href="https://twitter.com/kdsc_expo"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+        <div class="cloned">
+            <div class="cloned_nav"></div>
+        </div>
+    </header>
+    <section id="hero" class="bg_home bg_image overflow-hidden py-12">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2 text-center">
+                    <p class="fs-4 tc_primary">Impactful events.</p>
+                    <h1 class="mb-0">Our events connect you with top experts and career tips.</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="events" class="py-12">
+        <div class="container">
+            <ul class="list-unstyled event_filter d-flex justify-content-center mb-4 flex-wrap gap-2">
+                <li><a href="#!" data-filter="*" class="active">All Events</a></li>
+                <li><a href="#!" data-filter=".webinars">Webinars</a></li>
+                <li><a href="#!" data-filter=".physical">Physical</a></li>
+                <li><a href="#!" data-filter=".secondary">Secondary</a></li>
+                <li><a href="#!" data-filter=".tertiary">Tertiary</a></li>
+            </ul>
+            <div class="events_body row">
+                <div class="col-lg-6 event_outer webinars mb-4">
+                    <div class="event_two_box"><img src="img/webinar-flyer-1.jpg" class="img-fluid" alt=""> <i
+                            class="eb_icon bg_primary ph ph-webcam"></i> <a href="single-event.html"
+                            class="event_two_box_content">
+                            <p><i class="ph ph-map-pin-line"></i> Google Meet</p>
+                            <h3 class="fs-4">Crafting a compelling Career: CV vs Resume</h3>
+                            <h3 class="fs-4">- Afeez Bello</h3>
+                        </a></div>
+                </div>
+                <div class="col-lg-6 event_outer webinars mb-4">
+                    <div class="event_two_box"><img src="img/webinar-flyer-2.jpg" class="img-fluid" alt=""> <i
+                            class="eb_icon bg_primary ph ph-webcam"></i> <a href="single-event.html"
+                            class="event_two_box_content">
+                            <p><i class="ph ph-map-pin-line"></i> Google Meet</p>
+                            <h3 class="fs-4">Understanding the Future of Work</h3>
+                            <h3 class="fs-4">- Olaniyi Soyinka </h3>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="bg_gray py-12">
+        <div class="container">
+            <div class="col-lg-8 offset-lg-2 text-center">
+                <img src="img/kdsce-logo-cropped-no-bg.png" class="img-fluid logo mb-2" alt="kdsce_logo">
+                <ul
+                    class="list-unstyled fw-medium d-flex flex-column flex-md-row align-items-center justify-content-center gap-md-0 m-0 mb-4 gap-1">
+                    <li><a class="p-2" href="index.html">Home</a></li>
+                    <li><a class="p-2" href="about.html">About</a></li>
+                    <li><a class="p-2 tc_primary" href="events.html">Events</a></li>
+                    <li><a class="p-2" href="contact.html">Contact</a></li>
+                    <li><a class="p-2" href="">Blog</a></li>
+                </ul>
+                <ul class="list-unstyled footer_gray_social mb-12">
+                    <li><a href="https://www.linkedin.com/company/kaduna-student-career-expo"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li><a href="https://www.instagram.com/kaduna_student_career_expo"><i class="fa-brands fa-instagram"></i></a></li>
+                    <li><a href="https://twitter.com/kdsc_expo"><i class="fa-brands fa-x-twitter"></i></a></li>
+                </ul>
+                <h5>&copy; 2025 - KDSCE - Kaduna Student Career Expo.</h5>
+                <p>Built with ♥ by <a href="" class="text-decoration-underline">KingJoshua</a></p>
+            </div>
+        </div>
+    </footer>
+    <div class="scrollToTopBtn rounded-4 shadow-sm"><i class="ph ph-arrow-up"></i></div>
+    <script src="js/vendor.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
