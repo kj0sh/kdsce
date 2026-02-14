b04b69dd724dcb29b185a7ea57be621660c1ba04
KingJoshua <kingjayeriughe@gmail.com>
2026-02-14 19:28:43 +0100

    feat: add error page view

---
+++ App/views/error.view.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>KDSCE Event - Error Page</title>
+    <meta name="description" content="Evente - Conference and Event HTML Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/animate.min.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/flaticon_mycollection.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/odometer.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/default.css">
+    <link rel="stylesheet" href="https://cdn.kdsce.com.ng/events-assets/css/main.css">
+</head>
+
+<body>
+
+    <!-- Preloader Start -->
+    <div class="preloader">
+        <!-- <div class="loader"></div> -->
+    </div>
+    <!-- Preloader End -->
+
+    <!-- Scroll-top -->
+    <button class="scroll__top scroll-to-target" data-target="html">
+        <i class="fa-sharp fa-regular fa-arrow-up"></i>
+    </button>
+    <!-- Scroll-top-end-->
+
+    <!-- main-area -->
+    <main>
+
+        
+        <!-- td-error-area-start -->
+        <div class="td-error-area pt-180 pb-200">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <div class="td-error-wrap">
+                            <div class="td-error-thumb">
+                                <img src="assets/img/error/error.png" alt="">
+                            </div>
+                            <div class="text-center">
+                                <h2 class="td-error-title mb-20">Error Page!</h2>
+                                <p class="mb-30">Sorry! This Page is Not Available!</p>
+                                <a class="td-btn td-btn-3-squre" href="https://kdsce.com.ng">Go back to Home Page</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- td-error-area-end -->
+    </main>
+    <!-- main-area-end -->
+
+    <!-- JS here -->
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/vendor/jquery.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/bootstrap.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/isotope.pkgd.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/ion.rangeSlider.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/jquery.odometer.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/jquery-appear.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/swiper-bundle.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/Jarallax.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/nice-select.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/ajax-form.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/wow.min.js"></script>
+    <script src="https://cdn.kdsce.com.ng/events-assets/js/main.js"></script>
+</body>
+
+</html>
